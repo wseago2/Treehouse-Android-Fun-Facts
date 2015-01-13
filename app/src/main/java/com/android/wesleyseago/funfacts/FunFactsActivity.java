1da8bd139c0015fee068e705ace0e96172dd5efd
Wes Seago <wseago2@gmail.com>
2015-01-13 10:28:08 -0500

    Add OnClickListener.

--- app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,8 +17,18 @@ public class FunFactsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_fun_facts);
 
         // Declare our view variables and assign them the views from the layout file
-        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The button was clicked so update the factLabel with a new fact
+                String fact = "Ostriches can run faster than horses.";
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
