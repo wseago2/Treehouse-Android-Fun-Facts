cea9a9c9b3ae2b941cdfb020f9cdc7a90ccd6fee
Wes Seago <wseago2@gmail.com>
2015-01-13 11:30:31 -0500

    Add string array.

--- app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
@@ -25,13 +25,28 @@ public class FunFactsActivity extends ActionBarActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create string array
+                String[] facts = {
+                       "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach the Earth.",
+                        "Some bamboo plants can grow a meter in just 1 day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the Earth when the Great Pyramid was being built." };
+
                 // The button was clicked so update the factLabel with a new fact
                 String fact = "";
 
                 // Randomly select a fact
                 Random randomGenerator = new Random(); // Random constructor
-                int randomNumber = randomGenerator.nextInt(3);
-                fact = randomNumber + "";
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+                // Convert random number to a text fact
+                fact = facts[randomNumber];
 
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
