9f970ce09af06d3040687aeba7b93b396d8b4f44
Wes Seago <wseago2@gmail.com>
2015-01-13 10:18:40 -0500

    Add variables.

--- app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/android/wesleyseago/funfacts/FunFactsActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends ActionBarActivity {
@@ -12,6 +14,10 @@ public class FunFactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our view variables and assign them the views from the layout file
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
     }
 
 
